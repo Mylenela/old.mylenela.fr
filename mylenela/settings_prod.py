a1499a9b5b9d9fe6db0f86f38799b42ad7e4b3f1
Geoffrey Lehée <geoffrey@lehee.name>
2013-09-22 18:24:32 +0200

    Added redis cloud cache settings for production

--- mylenela/settings_prod.py
+++ mylenela/settings_prod.py
@@ -10,6 +10,16 @@ import dj_database_url
 DATABASES = {'default': dj_database_url.parse(os.environ.get(
     'DATABASE_URL'))}
 
+CACHES = {
+    "default": {
+        "BACKEND": "redis_cache.cache.RedisCache",
+        "LOCATION": os.environ.get('REDISCLOUD_URL'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
+        }
+    }
+}
+
 ALLOWED_HOSTS = [
     'www-mylenela-fr.herokuapp.com',
     'www.mylenela.fr',
