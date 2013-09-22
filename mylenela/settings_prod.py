0ecb9fd5a6c9ccb17e55df865d5f4230a8929302
Geoffrey Lehée <geoffrey@lehee.name>
2013-09-22 14:15:24 +0200

    added statics

--- mylenela/settings_prod.py
+++ mylenela/settings_prod.py
@@ -4,5 +4,7 @@ ENV = "prod"
 DEBUG = False
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_OLIVE_URL', 'postgres://socketubs:@localhost/mylenela'))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get(
+    'HEROKU_POSTGRESQL_AMBER_URL ',
+    'postgres://socketubs:@localhost/mylenela'))}
 ALLOWED_HOSTS = ['localhost']
