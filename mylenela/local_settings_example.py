5bd777014868f2cf143504fc1aff8f5aef97de1b
Geoffrey Lehée <socketubs@users.noreply.github.com>
2014-06-19 16:28:40 +0200

    Update local_settings_example.py

--- mylenela/local_settings_example.py
+++ mylenela/local_settings_example.py
@@ -3,3 +3,6 @@ from .settings import BASE_DIR
 from .settings import INSTALLED_APPS
 from .settings import MIDDLEWARE_CLASSES
 from .settings import TEMPLATE_CONTEXT_PROCESSORS
+
+DEBUG = True
+COMPRESS_OFFLINE = False
